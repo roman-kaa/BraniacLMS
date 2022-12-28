0d83af96417d16f825d6f474a6eaf8eec34d8889
roman.kaa <roman.kaa@gmail.com>
2022-12-28 17:56:50 +0000

    lesson_3 Template inhereting

--- config/settings.py
+++ config/settings.py
@@ -52,10 +52,25 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "config.urls"
 
+# TEMPLATES = [
+#     {
+#         "BACKEND": "django.template.backends.django.DjangoTemplates",
+#         "DIRS": [],
+#         "APP_DIRS": True,
+#         "OPTIONS": {
+#             "context_processors": [
+#                 "django.template.context_processors.debug",
+#                 "django.template.context_processors.request",
+#                 "django.contrib.auth.context_processors.auth",
+#                 "django.contrib.messages.context_processors.messages",
+#             ],
+#         },
+#     },
+# ]
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
