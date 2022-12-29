b10f455aab4914097722b39a81b4f061261e8ec4
roman.kaa <roman.kaa@gmail.com>
2022-12-29 19:22:45 +0000

    lesson_5 New app and user model

--- config/settings.py
+++ config/settings.py
@@ -157,3 +157,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,14 @@
+# from django.contrib import admin
+# from django.urls import include, path
+# from django.views.generic import RedirectView
+
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path("", RedirectView.as_view(url="mainapp/")),
+#     path("mainapp/", include("mainapp.urls", namespace="mainapp")),
+# ]
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
@@ -7,3 +18,6 @@ urlpatterns = [
     path("", RedirectView.as_view(url="mainapp/")),
     path("mainapp/", include("mainapp.urls", namespace="mainapp")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
