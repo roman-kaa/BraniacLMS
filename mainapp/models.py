6b0491cd2db73605304b893ef48d293847f2e691
roman.kaa <roman.kaa@gmail.com>
2022-12-29 22:28:56 +0000

    lesson_6 Customizing admin panel

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class News(models.Model):
@@ -17,23 +18,10 @@ class News(models.Model):
         self.deleted = True
         self.save()
 
-
-# class Courses(models.Model):
-#     name = models.CharField(max_length=256, verbose_name="Name")
-#     description = models.TextField(verbose_name="Description", blank=True, null=True)
-#     description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
-#     cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Cost", default=0)
-#     cover = models.CharField(max_length=25, default="no_image.svg", verbose_name="Cover")
-#     created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
-#     updated = models.DateTimeField(auto_now=True, verbose_name="Edited")
-#     deleted = models.BooleanField(default=False)
-
-#     def __str__(self) -> str:
-#         return f"{self.pk} {self.name}"
-
-#     def delete(self, *args):
-#         self.deleted = True
-#         self.save()
+    class Meta:
+        verbose_name = _("News")
+        verbose_name_plural = _("News")
+        ordering = ("-created",)
 
 
 class CoursesManager(models.Manager):
@@ -80,6 +68,8 @@ class Lesson(models.Model):
 
     class Meta:
         ordering = ("course", "num")
+        verbose_name = _("Lesson")
+        verbose_name_plural = _("Lessons")
 
 
 class CourseTeachers(models.Model):
@@ -95,3 +85,7 @@ class CourseTeachers(models.Model):
     def delete(self, *args):
         self.deleted = True
         self.save()
+
+    class Meta:
+        verbose_name = _("Teacher")
+        verbose_name_plural = _("Teachers")
