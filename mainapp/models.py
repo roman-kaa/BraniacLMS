2285f0db3a4477a6a723d65d821786b5d5f29e27
roman.kaa <85372568+roman-kaa@users.noreply.github.com>
2022-12-30 02:10:58 +0300

    Lesson 6 (#5)
    * lesson_6 Link to admin
    
    * lesson_6 Customizing admin panel
    
    * lesson_6 New user register form
    
    * lesson_6 User edit form
    
    * lesson_6 News CRUD and permissions
    
    * lesson_6 Feedbacks with AJAX

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class News(models.Model):
@@ -17,23 +19,10 @@ class News(models.Model):
         self.deleted = True
         self.save()
 
-
-# class Courses(models.Model):
-#     name = models.CharField(max_length=256, verbose_name="Name")
-#     description = models.TextField(verbose_name="Description", blank=True, null=True)
-#     description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
-#     cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Cost", default=0)
-#     cover = models.CharField(max_length=25, default="no_image.svg", verbose_name="Cover")
-#     created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
-#     updated = models.DateTimeField(auto_now=True, verbose_name="Edited")
-#     deleted = models.BooleanField(default=False)
-
-#     def __str__(self) -> str:
-#         return f"{self.pk} {self.name}"
-
-#     def delete(self, *args):
-#         self.deleted = True
-#         self.save()
+    class Meta:
+        verbose_name = _("News")
+        verbose_name_plural = _("News")
+        ordering = ("-created",)
 
 
 class CoursesManager(models.Manager):
@@ -61,6 +50,19 @@ class Courses(models.Model):
         self.save()
 
 
+class CourseFeedback(models.Model):
+    RATING = ((5, "⭐⭐⭐⭐⭐"), (4, "⭐⭐⭐⭐"), (3, "⭐⭐⭐"), (2, "⭐⭐"), (1, "⭐"))
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE, verbose_name=_("Course"))
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_("User"))
+    feedback = models.TextField(default=_("No feedback"), verbose_name=_("Feedback"))
+    rating = models.SmallIntegerField(choices=RATING, default=5, verbose_name=_("Rating"))
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.course} ({self.user})"
+
+
 class Lesson(models.Model):
     course = models.ForeignKey(Courses, on_delete=models.CASCADE)
     num = models.PositiveIntegerField(verbose_name="Lesson number")
@@ -80,6 +82,8 @@ class Lesson(models.Model):
 
     class Meta:
         ordering = ("course", "num")
+        verbose_name = _("Lesson")
+        verbose_name_plural = _("Lessons")
 
 
 class CourseTeachers(models.Model):
@@ -95,3 +99,7 @@ class CourseTeachers(models.Model):
     def delete(self, *args):
         self.deleted = True
         self.save()
+
+    class Meta:
+        verbose_name = _("Teacher")
+        verbose_name_plural = _("Teachers")
